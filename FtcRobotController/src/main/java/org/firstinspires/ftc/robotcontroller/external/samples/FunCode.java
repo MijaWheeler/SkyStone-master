90f4f9a0a11491c0a5b9e36c2dd5346c074d6da3
Kaikola <kaikodecker@gmail.com>
2019-09-23 16:00:03 -1000

    Initial commit

---
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/FunCode.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.robotcontroller.external.samples;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp
+
+public class FunCode extends BasicOpMode_Linear {
+    private DcMotor imu;
+
+@Override
+public void runOpMode(){}}
+
+
+
+
+
